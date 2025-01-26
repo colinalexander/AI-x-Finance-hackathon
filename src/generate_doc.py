00e36deadd897ac37e8851240144c56985119237
Iv Robinson <aerobinsoniv@gmail.com>
2025-01-25 17:37:57 -0800

    fix encoding on doc export

--- src/generate_doc.py
+++ src/generate_doc.py
@@ -5,10 +5,12 @@ ticker_string = f"#let ticker_symbol = \"{TICKER_SYMBOL}\""
 rec_string = f"#let recommendation = {REC}"
 
 full_substitution = ticker_string + "\n" + rec_string
-with open('src/doc_template.typ', 'r') as file:
+with open('src/doc_template.typ', 'rb') as file:
     doc_template = file.read()
 
-full_doc = doc_template.replace("//substitute", full_substitution)
+template_str = str(doc_template, encoding='utf-8')
 
-with open('output.typ', 'w') as file:
-    file.write(full_doc)
+full_doc = template_str.replace("//substitute", full_substitution)
+
+with open('output.typ', 'wb') as file:
+    file.write(full_doc.encode('utf-8'))
