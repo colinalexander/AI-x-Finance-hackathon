1d70b269a62778dc33d41079394d6dcca13fd9ba
Ruthvik Mukkamala <ruthvikmukkamala@gmail.com>
2025-01-25 18:42:20 -0800

    formatting

--- data/AIFDataLoader.py
+++
@@ -1,9 +0,0 @@
-from .extract import FinancialDataExtractor, RedditDataExtractor
-from .transform import DataTransformer
-from .load import DataLoader
-
-
-class AIFDataLoader:
-    def __init__(self):
-        ... 
-    
